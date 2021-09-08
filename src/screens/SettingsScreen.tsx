fc2742477fe8ce9081e64314b59e0f346b5000df
UnnamedXAer <unnamedxaer.git@gmail.com>
2021-09-08 22:49:26 +0200

    install slider package

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,30 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { Platform, StyleSheet, Switch } from 'react-native';
+import { Slider } from '@miblanchard/react-native-slider';
 import { useSelector, useDispatch } from 'react-redux';
 import Button from '../components/ui/Button';
 
 import { Text, View } from '../components/ui/Themed';
-import { restoreDefault, updateSettings } from '../store/exercise';
+import {
+  customizableExerciseStateProps,
+  restoreDefault,
+  updateSettings,
+} from '../store/exercise';
 import { RootState } from '../store/types';
+import {
+  ExerciseCustomizableProps,
+  ExerciseCustomizableState,
+} from '../store/exercise/types';
+import Layout from '../constants/Layout';
 
 export default function SettingsScreen() {
   const dispatch = useDispatch();
-  const disableAnimation = useSelector(
-    (state: RootState) => state.exercise.disableAnimation,
-  );
+
+  const exerciseConfig = useSelector((state: RootState) => {
+    const config = {} as ExerciseCustomizableState;
+
+    customizableExerciseStateProps.forEach(
+      <K extends ExerciseCustomizableProps>(prop: K) => {
+        config[prop] = state.exercise[prop];
+      },
+    );
+
+    return config;
+  });
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Settings</Text>
 
+      <Slider
+        value={exerciseConfig.numberOfRounds}
+        minimumValue={1}
+        maximumValue={10}
+        step={1}
+        onValueChange={(value) => {
+          dispatch(
+            updateSettings({ propName: 'numberOfRounds', value: value as number }),
+          );
+        }}
+      />
+
       <View style={styles.switchContainer}>
         <Text style={styles.switchLabel}>Disable animation</Text>
         <Switch
-          value={disableAnimation}
-          onValueChange={(ev) => {
-            console.log(ev);
-            dispatch(updateSettings({ propName: 'disableAnimation', value: ev }));
+          value={exerciseConfig.disableAnimation}
+          onValueChange={(value) => {
+            dispatch(updateSettings({ propName: 'disableAnimation', value }));
           }}
         />
       </View>
@@ -45,7 +75,7 @@ export default function SettingsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    paddingHorizontal: Layout.spacing(2),
     justifyContent: 'center',
   },
   title: {
