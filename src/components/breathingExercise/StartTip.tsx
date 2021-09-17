9339d8740bc9023aa095259f923c4c3e529eac63
UnnamedXAer <unnamedxaer.git@gmail.com>
2021-09-17 18:35:24 +0200

    fix start tip's position and bg color

--- src/components/breathingExercise/StartTip.tsx
+++ src/components/breathingExercise/StartTip.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import Colors from '../../constants/Colors';
 import Layout from '../../constants/Layout';
+import useColorScheme from '../../hooks/useColorScheme';
 import { Text } from '../ui/Themed';
 
 interface Props {
@@ -8,8 +10,16 @@ interface Props {
 }
 
 const StartTip: React.FC<Props> = ({ text }) => {
+  const scheme = useColorScheme();
+
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: Colors[scheme].backgroundRGBA(0.6),
+        },
+      ]}>
       <Text style={styles.text}>{text}</Text>
     </View>
   );
@@ -19,8 +29,12 @@ export default StartTip;
 
 const styles = StyleSheet.create({
   container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     flex: 1,
-    backgroundColor: 'rgba(250,250,250, 0.8)',
     justifyContent: 'center',
     alignItems: 'center',
   },
