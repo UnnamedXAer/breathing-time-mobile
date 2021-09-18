c66c1fcfefae6b7f214bbf1e00b2dc962973caf4
UnnamedXAer <unnamedxaer.git@gmail.com>
2021-09-18 10:01:23 +0200

    fill with content

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,35 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import * as React from 'react';
-import { Platform, StyleSheet } from 'react-native';
-import Separator from '../components/ui/Separator';
+import React from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import Headline from '../components/ui/Headline';
+import { Text } from '../components/ui/Themed';
+import Colors from '../constants/Colors';
+import Layout from '../constants/Layout';
+import useColorScheme from '../hooks/useColorScheme';
+import Constants from 'expo-constants';
 
-import { Text, View } from '../components/ui/Themed';
+const about =
+  'Breathing Time is a simple web application designed to help you with breathing exercises based on the Wim Hof method.\nIt allows you to easily adjust tempo of exercise based on your preferences. The breathing exercises are the first of the three pillars in the Wim Hof Method. You can find more information on the Wim Hof Method official website.';
 
 export default function AboutScreen() {
+  const scheme = useColorScheme();
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>About</Text>
-      <Separator />
+    <ScrollView style={styles.scroll} contentContainerStyle={styles.scrollContainer}>
+      <View>
+        <View style={styles.headerContainer}>
+          <Headline variant={'h2'} style={{ textAlign: 'center' }}>
+            About Application
+          </Headline>
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>{about}</Text>
+        </View>
+      </View>
 
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-    </View>
+      <View style={styles.footerContainer}>
+        <Text
+          style={{
+            color: Colors[scheme].textRGBA(0.7),
+          }}>
+          App version: {Constants.manifest?.version}
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  scroll: {
     flex: 1,
+  },
+  scrollContainer: {
+    flex: 1,
+    paddingHorizontal: Layout.spacing(3),
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  headerContainer: {
+    marginBottom: Layout.spacing(3),
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  textContainer: {
+    maxWidth: 420,
+  },
+  text: {
+    fontSize: Layout.spacing(2.2),
+    textAlign: 'justify',
+  },
+  footerContainer: {
+    marginVertical: Layout.spacing(2),
   },
 });
