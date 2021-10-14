b8ce47d4a7d720455823fc833501441e7cace22b
UnnamedXAer <unnamedxaer.git@gmail.com>
2021-10-14 21:30:41 +0200

    fix setting locale to Default work incorrectly #2

--- src/i18n/helpers.ts
+++ src/i18n/helpers.ts
@@ -5,18 +5,24 @@ import { Locales, Locale } from '../store/settings/types';
 
 export function getBestLocale() {
   const storeLocale = store.getState().settings.locale;
-  if (storeLocale !== 'default') {
+  if (storeLocale !== Locales.Default) {
     return storeLocale;
   }
+
+  return findLocaleInSystems() || Locales.EN;
+}
+
+function findLocaleInSystems() {
   const matchedLang = Object.values(Locales).find((l) => l === locale.substr(0, 2));
   if (matchedLang) {
     return matchedLang;
   }
-
-  return Locales.EN;
+  return null;
 }
 
 export function updateLocale(newLocale: Locale) {
-  const locale = newLocale === 'default' ? Locales.EN : newLocale;
+  const locale =
+    newLocale === Locales.Default ? findLocaleInSystems() || Locales.EN : newLocale;
+  console.log('new locale: ', newLocale, 'setting locale to:', locale);
   I18n.locale = locale;
 }
